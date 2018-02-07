4e1d4330b9f3a23179b9b87ec4cfb86998a5b12e
CruzMayra <velazquez.leonora@gmail.com>
2018-02-07 13:01:41 -0600

    Ínicializando Firebase

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBF3Q7Sg2rKOCriKyo6kCb20d4a7C0S-_w",
+  authDomain: "ticket-hack.firebaseapp.com",
+  databaseURL: "https://ticket-hack.firebaseio.com",
+  projectId: "ticket-hack",
+  storageBucket: "ticket-hack.appspot.com",
+  messagingSenderId: "642426168856"
+};
+firebase.initializeApp(config);
+
+
+  // función que centraliza al resto de las funciones
+function loadPage() {
+  loadSplashView();
+  loadMainView();
+ }
+
 //Función que hace desaparecer mi imagen principal
-$(document).ready(function() {
+function loadSplashView() {
   setTimeout(function() {
       $("#view-splash").fadeOut(1500);
     },2000);
-});
+};
 
 //Función que hace aparecer la siguiente pantalla
-$(document).ready(function() {
+function loadMainView() {
     setTimeout(function() {
       $("#second-section").fadeIn(1500);
     },2000);
-});
+};
+
+
+
+
+$(document).ready(loadPage);
