24f9eddc5c160d93e939223c98b307a67c30b1f7
ximada <witch_ada@hotmail.com>
2018-02-10 23:23:59 -0600

    agregando funcionalidad rating

--- js/app.js
+++ js/app.js
@@ -236,5 +236,21 @@ function printEvents(nameEvent,infoEvent,dateEvent,timeEvent,adressEvent,urlEven
 
 }
 
+  //Función de estrellas
+  var $star_rating = $('.star-rating .fa');
+  var SetRatingStar = function () {
+    return $star_rating.each(function () {
+      if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+        return $(this).removeClass('fa-star-o').addClass('fa-star');
+      } else {
+        return $(this).removeClass('fa-star').addClass('fa-star-o');
+      }
+    });
+  }; //termina función setrating
+  $star_rating.on('click', function () {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+  }); 
+
 $(document).ready(loadPage);
 $(document).ready(loadHomePage);
